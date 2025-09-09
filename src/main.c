9e7aaac66577bc5022840cd2132427e5f95623df
Andres <andresalfaro.am@gmail.com>
2025-09-08 22:19:34 -0600

    SCRUM-28 Aplicar padding

--- src/main.c
+++ src/main.c
@@ -4,10 +4,21 @@
 #define HEIGHT 768
 
 
-uint64_t* string_to_blocks64(const char *str, size_t *out_blocks) {
+uint8_t* apply_padding(const uint8_t *bytes, size_t len, size_t *out_len) {
+  size_t pad = (len % 8 == 0) ? 0 : 8 - (len % 8);
+  *out_len = len + pad;
+
+  uint8_t *padded = calloc(*out_len, 1);
+  if (!padded) return NULL;
+
+  memcpy(padded, bytes, len);
+
+  return padded;
+}
+
+uint64_t* string_to_blocks64(const uint8_t *bytes,  size_t len, size_t *out_blocks) {
   // Calculate number of blocks
-  size_t str_len = strlen(str);
-  size_t n_blocks = str_len / 8;
+  size_t n_blocks = len / 8;
 
   // Reserve memory
   uint64_t *blocks = malloc(n_blocks * sizeof(u_int64_t));
@@ -17,11 +28,12 @@ uint64_t* string_to_blocks64(const char *str, size_t *out_blocks) {
     return NULL;
   }
 
-  // Create blocks
+  // Create blocks (Big endian)
   for (size_t i = 0; i < n_blocks; i++) {
     uint64_t block = 0;
     for (int j = 0; j < 8; j++) {
-      block |= ((uint64_t)(uint8_t)str[i*8 + j]) << (j * 8);
+      block <<= 8;
+      block |= ((uint64_t)bytes[i*8 + j]);
     }
     blocks[i] = block;
   }
@@ -41,15 +53,25 @@ static void on_button_encrypt(GtkButton *btn, gpointer user_data) {
     return;
   }
 
+  // ASCII to Bytes
+  uint8_t *bytes = (const uint8_t *)msg;
+  size_t len = strlen(msg);
+
+  // Apply padding
+  size_t padded_len = 0;
+  uint8_t *padded = apply_padding(bytes, len, &padded_len);
+
   // Divide in blocks of 64 bits
   size_t n_blocks = 0;
-  uint64_t* blocks = string_to_blocks64(msg, &n_blocks);
+  uint64_t *blocks = string_to_blocks64(padded, padded_len, &n_blocks);
 
   for  (size_t i = 0; i < n_blocks; i++) {
     g_print("Block %zu: 0x%016lX\n", i, blocks[i]);
   }
 
+  free(padded);
   free(blocks);
+  g_free((gchar *)msg);
 }
 
 static void on_button_decode(GtkButton *btn, gpointer user_data) {
