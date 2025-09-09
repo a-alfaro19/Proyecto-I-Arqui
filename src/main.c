77653d548e69ed523ffdda5728ef9dfbd1b3573a
Andres <andresalfaro.am@gmail.com>
2025-09-09 16:31:32 -0600

    SCRUM-39 Cambiar UI a menu de consola

--- src/main.c
+++ src/main.c
@@ -1,35 +1,152 @@
-#include <gtk/gtk.h>
+#include <stdint.h>
 
-#define WIDTH 1200
-#define HEIGHT 768
+// Assembly function declaration
+// extern void tea_encrypt(uint32_t v[2], const uint32_t key[4]);
+
+// Simple implementation of basic functions since we're in bare-metal environment
+void print_char(char c) {
+    // In a real bare-metal environment, this would write to UART
+    // For now, this is just a placeholder
+    volatile char *uart = (volatile char*)0x10000000;
+    *uart = c;
+}
+
+void print_number(int num) {
+    if (num == 0) {
+        print_char('0');
+        return;
+    }
+    
+    if (num < 0) {
+        print_char('-');
+        num = -num;
+    }
+    
+    char buffer[10];
+    int i = 0;
+    
+    while (num > 0) {
+        buffer[i++] = '0' + (num % 10);
+        num /= 10;
+    }
+    
+    // Print digits in reverse order
+    while (i > 0) {
+        print_char(buffer[--i]);
+    }
+}
+
+void print_string(const char* str) {
+    while (*str) {
+        print_char(*str++);
+    }
+}
+
+void print_hex32(uint32_t num) {
+    const char *hex = "0123456789ABCDEF";
+    print_string("0x");
+    for (int i = 28; i >= 0; i -= 4) {
+        print_char(hex[(num >> i) & 0xF]);
+    }
+}
+
+void print_hex64(uint64_t num) {
+    const char *hex = "0123456789ABCDEF";
+    print_string("0x");
+    for (int i = 60; i >= 0; i -= 4) {
+        print_char(hex[(num >> i) & 0xF]);
+    }
+}
+
+void print_byte(uint8_t b) {
+    const char *hex = "0123456789ABCDEF";
+    print_char(hex[(b >> 4) & 0xF]);
+    print_char(hex[b & 0xF]);
+}
+
+void print_bytes(const uint8_t *data, int len) {
+    for (int i = 0; i < len; i++) {
+        print_byte(data[i]);
+        print_char(' ');  // espacio entre bytes
+    }
+    print_char('\n');
+}
+
+#define HEAP_SIZE 4096
+static unsigned char heap[HEAP_SIZE];
+static unsigned int heap_index = 0;
+
+void *malloc(unsigned int size) {
+  if (heap_index + size > HEAP_SIZE) {
+    return 0;
+  }
+
+  void *ptr = &heap[heap_index];
+  heap_index += size;
+
+  return ptr;
+}
+
+void *calloc(unsigned int num, unsigned int size) {
+  unsigned int total = num * size;
+  unsigned char *ptr = malloc(total);
+
+  if (!ptr) return 0;
+
+  for (unsigned int i = 0; i < total; i++) {
+    ptr[i] = 0;
+  }
+
+  return ptr;
+}
+
+void free(void *ptr) {
+  (void)ptr;
+}
+
+void *memcpy(void *dest, const void *src, unsigned int n) {
+    unsigned char *d = dest;
+    const unsigned char *s = src;
+    while (n--) {
+        *d++ = *s++;
+    }
+    return dest;
+}
+
+int strlen(const char *s) {
+    int len = 0;
+    while (*s++) len++;
+    return len;
+}
 
 
-uint8_t* apply_padding(const uint8_t *bytes, size_t len, size_t *out_len) {
-  size_t pad = (len % 8 == 0) ? 0 : 8 - (len % 8);
+// Cryptographic functions
+uint8_t* apply_padding(const uint8_t *bytes, int len, int *out_len) {
+  int pad = (len % 8 == 0) ? 0 : 8 - (len % 8);
   *out_len = len + pad;
 
   uint8_t *padded = calloc(*out_len, 1);
-  if (!padded) return NULL;
+  if (!padded) return 0;
 
   memcpy(padded, bytes, len);
 
   return padded;
 }
 
-uint64_t* string_to_blocks64(const uint8_t *bytes,  size_t len, size_t *out_blocks) {
+uint64_t* string_to_blocks64(const uint8_t *bytes, int len, int *out_blocks) {
   // Calculate number of blocks
-  size_t n_blocks = len / 8;
+  int n_blocks = len / 8;
 
-  // Reserve memory
-  uint64_t *blocks = malloc(n_blocks * sizeof(u_int64_t));
+  // Reserve space in memory
+  uint64_t *blocks = malloc(n_blocks * sizeof(uint64_t));
 
   if (!blocks) {
     *out_blocks = 0;
-    return NULL;
+    return 0;
   }
 
   // Create blocks (Big endian)
-  for (size_t i = 0; i < n_blocks; i++) {
+  for (int i = 0; i < n_blocks; i++) {
     uint64_t block = 0;
     for (int j = 0; j < 8; j++) {
       block <<= 8;
@@ -44,104 +161,41 @@ uint64_t* string_to_blocks64(const uint8_t *bytes,  size_t len, size_t *out_bloc
   return blocks;
 }
 
-static void on_button_encrypt(GtkButton *btn, gpointer user_data) {
-  GtkComboBoxText *combo = GTK_COMBO_BOX_TEXT(user_data);
-  const char *msg = gtk_combo_box_text_get_active_text(combo);
 
-  if (!msg) {
-    g_print("No option selected!\n");
-    return;
-  }
+void main(int argc, char *argv[]) {
+  // Key
+  const uint32_t key[4] = {0x01234567, 0x89ABCDEF, 0xFEDCBA98, 0x76543210};
+
+  // String
+  const char* str = "HOLA1234";
+
+  // Program Header
+  print_string("=== Tiny Encryption Algorithm (TEA) ===\n");
 
   // ASCII to Bytes
-  uint8_t *bytes = (const uint8_t *)msg;
-  size_t len = strlen(msg);
+  uint8_t *bytes = (uint8_t *)str;
+  int len = strlen(str);
 
   // Apply padding
-  size_t padded_len = 0;
+  int padded_len = 0;
   uint8_t *padded = apply_padding(bytes, len, &padded_len);
 
   // Divide in blocks of 64 bits
-  size_t n_blocks = 0;
+  int n_blocks = 0;
   uint64_t *blocks = string_to_blocks64(padded, padded_len, &n_blocks);
 
-  for  (size_t i = 0; i < n_blocks; i++) {
-    g_print("Block %zu: 0x%016lX\n", i, blocks[i]);
+  // Debug
+  for (int i = 0; i < n_blocks; i++) {
+    print_string("Block ");
+    print_number(i);
+    print_string(": ");
+    print_hex64(blocks[i]);
+    print_char('\n');
   }
 
-  free(padded);
-  free(blocks);
-  g_free((gchar *)msg);
-}
 
-static void on_button_decode(GtkButton *btn, gpointer user_data) {
-  GtkComboBoxText *combo = GTK_COMBO_BOX_TEXT(user_data);
-  const char *option = gtk_combo_box_text_get_active_text(combo);
-
-
-  g_print("Decoding: %s\n", option);
-}
-
-int main(int argc, char *argv[]) {
-  GtkWidget *window;
-
-  // Initialize GTK
-  gtk_init (&argc, &argv);
-    
-  // Create window
-  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_title (GTK_WINDOW (window), "TEA Encryption");
-  gtk_window_set_default_size(GTK_WINDOW(window), WIDTH, HEIGHT);
-  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-  // Main Vertical Container
-  GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 8);
-  gtk_container_set_border_width(GTK_CONTAINER(vbox), 8);
-  gtk_container_add(GTK_CONTAINER(window), vbox);
-
-  // Encrypt Combo Label
-  GtkWidget *encrypt_combo_label = gtk_label_new("Selecciona una cadena para cifrar:");
-  gtk_box_pack_start(GTK_BOX(vbox), encrypt_combo_label, FALSE, FALSE, 0);
-
-  // Combo Box for Encryption
-  GtkWidget *encrypt_combo = gtk_combo_box_text_new();
-  gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(encrypt_combo), "HOLA1234");
-  gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(encrypt_combo), "Mensaje de prueba para TEA");
-  gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(encrypt_combo), "Hello, World!");
-  gtk_combo_box_set_active(GTK_COMBO_BOX(encrypt_combo), 0);
-  gtk_box_pack_start(GTK_BOX(vbox), encrypt_combo, FALSE, FALSE, 0);
-
-  // Encrypt Button
-  GtkWidget *btn_encrypt = gtk_button_new_with_label("Encrypt");
-  g_signal_connect(btn_encrypt, "clicked", G_CALLBACK(on_button_encrypt), encrypt_combo);
-  gtk_box_pack_start(GTK_BOX(vbox), btn_encrypt, FALSE, FALSE, 0);
-
-  // Separator
-  GtkWidget *sep = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);
-  gtk_box_pack_start(GTK_BOX(vbox), sep, FALSE, FALSE, 0);
-
-  // Decode Combo Label
-  GtkWidget *decode_combo_label = gtk_label_new("Selecciona una cadena para descifrar:");
-  gtk_box_pack_start(GTK_BOX(vbox), decode_combo_label, FALSE, FALSE, 0);
-
-  // Combo Box for Encryption
-  GtkWidget *decode_combo = gtk_combo_box_text_new();
-  gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(decode_combo), "HOLA1234");
-  gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(decode_combo), "Mensaje de prueba para TEA");
-  gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(decode_combo), "Hello, World!");
-  gtk_combo_box_set_active(GTK_COMBO_BOX(decode_combo), 0);
-  gtk_box_pack_start(GTK_BOX(vbox), decode_combo, FALSE, FALSE, 0);
-
-  // Encrypt Button
-  GtkWidget *btn_decode = gtk_button_new_with_label("Decode");
-  g_signal_connect(btn_decode, "clicked", G_CALLBACK(on_button_decode), decode_combo);
-  gtk_box_pack_start(GTK_BOX(vbox), btn_decode, FALSE, FALSE, 0);
-
-  // Show window's widgets
-  gtk_widget_show_all(window);
-
-  // Run UI
-  gtk_main();
-
-  return 0;
+  // Infinite loop to keep program running
+  while (1) {
+      __asm__ volatile ("nop");
+  }
 }
